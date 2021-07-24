7fd678fd4c6f839eb8b870905bddf6cb4f963e7b
leilamamedova <71094676+leilamamedova@users.noreply.github.com>
2021-07-24 15:17:51 +0400

    error js

--- src/components/error/error.js
+++ src/components/error/error.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import "./error";
 
 const Error = () => {
-    return <div className="error">Error</div>
+    return (
+        <div id="error">
+            <div className="error">Error</div>
+        </div>
+    )
 }
 
 export default Error;
